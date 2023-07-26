21f22221e5694e9b34bc511e0b2d1481f9f1cf94
Tom Slatter <tjs98_@outlook.com>
2023-07-26 13:51:09 +0100

    added log in form and basic login function (to be adapted to use api)

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-function LogInPage(){
+function LogInPage() {
+    const[email, setEmail] = useState("");
+    const[password, setPassword] = useState("");
+
+    async function LogIn() {
+        console.log(email, password);
+    }
+
     return (
         <div>
             <h1>Sign In</h1>
+            <div className="form-group">
+                <input type="text" placeholder="Username or Email address" onChange={(e)=>setEmail(e.target.value)} />
+            </div>
+            <div className="form-group">
+                <input type="password" placeholder="Password" onChange={(e)=>setPassword(e.target.value)} />
+            </div>
+            <div className="form-group">
+                <button className="btn btn-primary" onClick={LogIn}>Sign In</button>
+            </div>
         </div>
     );
 }
