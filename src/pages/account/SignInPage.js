e038280d4bb013aab3df015f83fb54746339dff3
Tom Slatter <tjs98_@outlook.com>
2023-07-27 18:08:53 +0100

    restructured signin page to use const instead of function

--- src/pages/account/SignInPage.js
+++ src/pages/account/SignInPage.js
@@ -5,7 +5,7 @@ import axios from '../../api/axios';
 
 import { Link } from 'react-router-dom';
 
-function SignInPage() {
+const SignInPage = () => {
     const { setAuth } = useContext(AuthContext);
 
     const usernameRef = useRef();
