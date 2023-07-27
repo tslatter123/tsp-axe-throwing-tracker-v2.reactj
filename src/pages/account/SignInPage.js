71d0463ca964f4417316f4c3817c2dd6b818792e
Tom Slatter <tjs98_@outlook.com>
2023-07-27 17:41:25 +0100

    Added check for successful login (no api)

--- src/pages/account/SignInPage.js
+++ src/pages/account/SignInPage.js
@@ -1,4 +1,5 @@
 import React, { useRef, useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 
 function SignInPage() {
     const usernameRef = useRef();
@@ -7,7 +8,7 @@ function SignInPage() {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
     const [errorMsg, setErrorMsg] = useState('');
-    const [success, setSuccess] = useState('');
+    const [success, setSuccess] = useState(false);
 
     useEffect(() => {
         usernameRef.current.focus();
@@ -20,39 +21,49 @@ function SignInPage() {
     const handleSubmit = async (e) => {
         e.preventDefault();
         console.log(username, password);
+        setSuccess(true);
     }
 
     return (
-        <section>
-            <p ref={errorRef} aria-live='assertive'>{errorMsg}</p>
-            <h1>Sign In</h1>
-            <form id='loginForm' onSubmit={handleSubmit}>
-                <div className="form-group">
-                    <input
-                        id='username'
-                        type='text'
-                        placeholder='Username or Email address'
-                        ref={usernameRef}
-                        onChange={(e) => setUsername(e.target.value)}
-                        value={username}
-                        required
-                    />
-                </div>
-                <div className="form-group">
-                    <input
-                        id='password'
-                        type='password'
-                        placeholder='Password'
-                        onChange={(e) => setPassword(e.target.value)}
-                        value={password}
-                        required
-                    />
-                </div>
-                <div className="form-group">
-                    <button>Sign In</button>
-                </div>
-            </form>
-        </section>
+        <>
+            {success ? (
+                <section>
+                    <p>Logged in successfully.</p>
+                    <Link to='/'>Back to Home</Link>
+                </section>
+            ) : (
+                <section>
+                    <p ref={errorRef} aria-live='assertive'>{errorMsg}</p>
+                    <h1>Sign In</h1>
+                    <form id='loginForm' onSubmit={handleSubmit}>
+                        <div className="form-group">
+                            <input
+                                id='username'
+                                type='text'
+                                placeholder='Username or Email address'
+                                ref={usernameRef}
+                                onChange={(e) => setUsername(e.target.value)}
+                                value={username}
+                                required
+                            />
+                        </div>
+                        <div className="form-group">
+                            <input
+                                id='password'
+                                type='password'
+                                placeholder='Password'
+                                onChange={(e) => setPassword(e.target.value)}
+                                value={password}
+                                required
+                            />
+                        </div>
+                        <div className="form-group">
+                            <button>Sign In</button>
+                        </div>
+                    </form>
+                </section>
+            )}
+        </>
     );
 }
 
