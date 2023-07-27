3f8f8b6e9adc84419ca83ffc78ef0dd59e1abd9a
Tom Slatter <tjs98_@outlook.com>
2023-07-27 15:50:17 +0100

    initial for axios login form setup

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,21 +1,34 @@
-import React, { useState } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 
 function LogInPage() {
-    const[email, setEmail] = useState("");
-    const[password, setPassword] = useState("");
+    const userRef = useRef();
+    const errorRef = useRef();
+
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [errorMsg, setErrorMsg] = useState('');
+    const [success, setSuccess] = useState('');
 
     async function LogIn() {
-        console.log(email, password);
+        console.log(username, password);
     }
+
+    useEffect(() => {
+        userRef.current.focus();
+    }, []);
+
+    useEffect(() => {
+        setErrorMsg('');
+    }, [username, password]);
 
     return (
         <div>
             <h1>Sign In</h1>
             <div className="form-group">
-                <input type="text" placeholder="Username or Email address" onChange={(e)=>setEmail(e.target.value)} />
+                <input type="text" placeholder="Username or Email address" onChange={(e) => setEmail(e.target.value)} />
             </div>
             <div className="form-group">
-                <input type="password" placeholder="Password" onChange={(e)=>setPassword(e.target.value)} />
+                <input type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
             </div>
             <div className="form-group">
                 <button className="btn btn-primary" onClick={LogIn}>Sign In</button>
