2858b0bedabe927afb2ef473c6f3211f3855449c
Tom Slatter <tjs98_@outlook.com>
2023-07-27 17:56:46 +0100

    setauth assigned in signin page

--- src/pages/account/SignInPage.js
+++ src/pages/account/SignInPage.js
@@ -4,6 +4,8 @@ import AuthContext from '../../context/AuthProvider';
 import { Link } from 'react-router-dom';
 
 function SignInPage() {
+    const { setAuth } = useContext(AuthContext);
+
     const usernameRef = useRef();
     const errorRef = useRef();
 
