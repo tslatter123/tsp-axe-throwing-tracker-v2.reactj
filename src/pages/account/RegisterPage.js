9cb3d16a4061058a513f9e91f8897f5e4e87bf57
Tom Slatter <tjs98_@outlook.com>
2023-07-31 15:41:50 +0100

    Added register form with some validation

--- src/pages/account/RegisterPage.js
+++ src/pages/account/RegisterPage.js
@@ -26,10 +26,76 @@ const RegisterPage = () => {
         setErrorMsg('');
     }, [username, email, confirmEmail, password, confirmPassword]);
 
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        if (confirmEmail !== email) {
+            setErrorMsg('Emails do not match. Please try again.');
+            return;
+        }
+        else if (confirmPassword !== password) {
+            setErrorMsg('Passwords do not match. Please try again.');
+            return;
+        }
+    }
+
     return (
         <>
             <section>
-                <p ref={errorRef}></p>
+                <p ref={errorRef} aria-live='assertive'>{errorMsg}</p>
+                <h1>Register</h1>
+                <form id='registerForm' onSubmit={handleSubmit}>
+                    <div className='form-group'>
+                        <input
+                            id='username'
+                            type='text'
+                            placeholder='Username'
+                            ref={usernameRef}
+                            onChange={(e) => setUsername(e.target.value)}
+                            value={username}
+                            required />
+                    </div>
+                    <div className='form-group'>
+                        <input
+                            id='email'
+                            type='email'
+                            placeholder='Email address'
+                            ref={emailRef}
+                            onChange={(e) => setEmail(e.target.value)}
+                            value={email}
+                            required />
+                    </div>
+                    <div className='form-group'>
+                        <input
+                            id='confirmEmail'
+                            type='email'
+                            placeholder='Confirm email address'
+                            onChange={(e) => setConfirmEmail(e.target.value)}
+                            value={confirmEmail}
+                            required />
+                    </div>
+                    <div className='form-group'>
+                        <input
+                            id='password'
+                            type='password'
+                            placeholder='Password'
+                            onChange={(e) => setPassword(e.target.value)}
+                            value={password}
+                            required />
+                    </div>
+                    <div className='form-group'>
+                        <input
+                            id='confirmPassword'
+                            type='password'
+                            placeholder='Confirm password'
+                            onChange={(e) => setPassword(e.target.value)}
+                            value={confirmPassword}
+                            required />
+                    </div>
+                    <div className='form-group'>
+                        <button>Register</button>
+                    </div>
+                </form>
             </section>
         </>
     );
