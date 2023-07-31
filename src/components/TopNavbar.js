d853f04efd2a216f809b9d410961f35853e99744
Tom Slatter <tjs98_@outlook.com>
2023-07-31 19:56:53 +0100

    css for top navabr

--- src/components/TopNavbar.js
+++ src/components/TopNavbar.js
@@ -1,14 +1,15 @@
+import '../assets/css/TopNavBar.css';
 import React from 'react';
 import { Link } from 'react-router-dom';
 
 function TopNavBar() {
     return (
         <header className='navbar navbar-top'>
-            <ul className='navbar-left'>
+            <ul className='nav-items navbar-left'>
                 <li><Link className="nav-link nav-title" to="/">Tom's Axe Throwing Tracker</Link></li>
                 <li><Link className="nav-link" to="/routetest">React Router Test</Link></li>
             </ul>
-            <ul className='navbar-right'>
+            <ul className='nav-items navbar-right'>
                 <li><Link className="nav-link" to="/signin">Sign In</Link></li>
                 <li><Link className="nav-link" to="/register">Register</Link></li>
             </ul>
