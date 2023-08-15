9c62197d5ed1c6edaaf1d5834157ed9642078229
Tom Slatter <tjs98_@outlook.com>
2023-08-15 11:23:19 +0100

    added useaxiosprivate hook

---
+++ src/hooks/useAxiosPrivate.js
@@ -0,0 +1,13 @@
+import { axiosPrivate } from "../api/axios";
+import { useEffect } from "react";
+import useRefreshToken from "./useRefreshToken";
+import useAuth from "./useAuth";
+
+const useAxiosPrivate = () => {
+    const refresh = useRefreshToken();
+    const { auth } = useAuth();
+
+    return axiosPrivate;
+}
+
+export default useAxiosPrivate;
