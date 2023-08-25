54e5a9848bf23fada201a172e4a321711dcf1382
Tom Slatter <tjs98_@outlook.com>
2023-08-25 09:31:57 +0100

    api call to fetch list of axes now working

--- src/pages/userAxes/AxesPage.js
+++ src/pages/userAxes/AxesPage.js
@@ -1,7 +1,37 @@
+import { useEffect } from "react";
+import useAxiosPrivate from "../../hooks/useAxiosPrivate";
+
+const userAxesUrl = 'UserAxes';
+
 const UserAxes = () => {
+    const axiosPrivate = useAxiosPrivate();
+
+    var userAxes = [];
+
+    useEffect(() => {
+        let isMounted = true;
+        const controller = new AbortController();
+
+        const getUserAxes = async () => {
+            try {
+                const response = await axiosPrivate.get(userAxesUrl, {
+                    signal: controller.signal
+                });
+
+                console.log(response.data);
+                isMounted && (userAxes = response.data.axeInfoList);
+            } catch (err) {
+                console.error(err);
+            }
+        };
+
+        getUserAxes();
+    }, []);
+
     return (
         <section>
             <h1>Your Saved Axes</h1>
+            {userAxes}
         </section>
     )
 }
