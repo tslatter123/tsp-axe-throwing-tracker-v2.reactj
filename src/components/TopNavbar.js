795a5bd7cc9b6b3720f26b913f4d3bc1dbd73341
Tom Slatter <tjs98_@outlook.com>
2023-08-25 09:12:22 +0100

    axes page link now only appears in header if user is signed in

--- src/components/TopNavbar.js
+++ src/components/TopNavbar.js
@@ -1,21 +1,29 @@
 import '../assets/css/TopNavBar.css';
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import SignInHeader from './SignInHeader';
 
 const TopNavBar = () => {
-    const getData = (data) => {
-        console.log("isSignedIn", data);
-    }
 
+    const [signedIn, setSignedIn] = useState(false);
+
+    const getData = (userSignedIn) => {
+        setSignedIn(userSignedIn);
+    };
 
     return (
         <header className='navbar navbar-top'>
             <ul className='nav-items navbar-left'>
                 <li><Link className="nav-link nav-title" to="/">TATT</Link></li>
                 <li><Link className="nav-link" to="/routetest">React Router Test</Link></li>
-                <li><Link className="nav-link" to="/axes">Axes</Link></li>
+                {signedIn ? (
+                    <>
+                        <li><Link className="nav-link" to="/axes">Axes</Link></li>
+                    </>
+                ) : (
+                    <></>
+                )}
             </ul>
             <SignInHeader onSubmit={getData} />
         </header>
