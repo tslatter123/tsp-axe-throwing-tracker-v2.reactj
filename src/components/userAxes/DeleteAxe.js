8b473b5fa87580e97b06d9e1533e6d7fb81bf296
Tom Slatter <tjs98_@outlook.com>
2023-08-31 00:19:27 +0100

    Added deleteaxe componenet

---
+++ src/components/userAxes/DeleteAxe.js
@@ -0,0 +1,60 @@
+import { useEffect, useRef, useState } from "react"
+import useAxiosPrivate from "../../hooks/useAxiosPrivate";
+
+const userAxeUrl = "UserAxe";
+
+const DeleteAxe = (props) => {
+    const [name, setName] = useState("");
+    const [description, setDescription] = useState("");
+
+    const axiosPrivate = useAxiosPrivate();
+
+    useEffect(() => {
+        let isMounted = true;
+        const controller = new AbortController();
+
+        const getAxeInfo = async () => {
+            try {
+                const response = await axiosPrivate.get(userAxeUrl + "?id=" + props.id, {
+                    signal: controller.signal
+                });
+                console.log(response.data);
+                if (isMounted) {
+                    setName(response.data.axeInfo.name);
+                    setDescription(response.data.axeInfo.description);
+                }
+            } catch (err) {
+                console.log(err);
+            }
+        }
+
+        getAxeInfo();
+    }, [axiosPrivate, props.id]);
+
+    const handleDelete = async (e) => {
+        e.preventDefault();
+
+        try {
+            await axiosPrivate.delete(userAxeUrl + "?id=" + props.id);
+
+            window.location.reload();
+        } catch (err) {
+            if (!err?.response) {
+                setErrorMsg("No server response.");
+            }
+            else {
+                setErrorMsg(err.response.data.error);
+            }
+        }
+    }
+
+    return (
+        <div className="popout popout-right">
+            <h2>Are you sure you want to delete {name}</h2>
+            <p>{description}</p>
+            <button onClick={handleDelete}>Delete</button>
+        </div>
+    );
+}
+
+export default DeleteAxe;
