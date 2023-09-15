e40d684265addf446ce88e155d3cba76deb5cc2b
Tom Slatter <tjs98_@outlook.com>
2023-09-15 16:05:12 +0100

    fixed api call issue

--- src/pages/userWatlGames/UserWatlGameAnalytics.js
+++ src/pages/userWatlGames/UserWatlGameAnalytics.js
@@ -7,7 +7,15 @@ const userWatlGameAnalyticsUrl = 'UserWatlGameAnalytics';
 const UserWatlGameAnalytics = () => {
     const errorMsgRef = useRef();
 
-    const [analyticsInfo, setAnalyticsInfo] = useState({});
+    const [average, setAverage] = useState(0);
+    const [scoreBreakdown, setScoreBreakdown] = useState([]);
+    const [mostCommonScore, setMostCommonScore] = useState(0);
+    const [inconsistencies, setInconsistencies] = useState([]);
+    const [consistenctThrowCount, setConsistentThrowCount] = useState(0);
+    const [totalThrowCount, setTotalThrowCount] = useState(0);
+    const [consistentThrowPercentage, setConsistentThrowPercentage] = useState(0);
+    const [accuracies, setAccuracies] = useState([]);
+
     const [errorMsg, setErrorMsg] = useState('');
 
     const axiosPrivate = useAxiosPrivate();
@@ -21,8 +29,10 @@ const UserWatlGameAnalytics = () => {
                 const response = await axiosPrivate.get(userWatlGameAnalyticsUrl, {
                     signal: controller.signal
                 });
-                debugger;
-                isMounted && setAnalyticsInfo(response.data.analyticsInfo);
+
+                if (isMounted) {
+
+                }
             } catch (err) {
                 if (!err?.response) {
                     setErrorMsg("No server response.");
@@ -44,7 +54,7 @@ const UserWatlGameAnalytics = () => {
                     <p ref={errorMsgRef} aria-live="assertive" className="error-msg">{errorMsg}</p>
                 ) : (
                     <div className="analytics-container">
-                        {analyticsInfo.accuracies.Map(accuracyItem => <AccuracyInfo accuracyInfo={accuracyItem} />)}
+                        {/* {analyticsInfo.accuracies.Map(accuracyItem => <AccuracyInfo accuracyInfo={accuracyItem} />)} */}
                     </div>
                 )}
             </section>
